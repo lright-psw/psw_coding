c80bc27c047fb32d6a023eda5a1d0389a200bc2c
lright-psw <52391568+lright-psw@users.noreply.github.com>
2023-01-17 20:22:34 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,15 @@
+# 아니 수학을 몰라서 어렵네 등차수열이 뭔데 10덕아
+# 연속하는 두 항의 차이가 모두 일정한 수열을 뜻한다. 
+# 10부터 99까지는 전부 등차수열임
+# 1~9까지는 그 자체로 수열임
+
+num = int(input())
+sum = 0
+for i in range(1,num+1):
+    ptr=list(map(int,str(i)))
+    if i < 100:
+        sum += 1
+    elif ptr[1]-ptr[0] == ptr[2]-ptr[1]:
+        sum += 1
+        
+print(sum)
