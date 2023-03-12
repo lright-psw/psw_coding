2ae9d4568763f8b87713de1a9093860f97ac4239
lright-psw <52391568+lright-psw@users.noreply.github.com>
2023-03-12 18:52:45 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,22 +1,18 @@
 import sys
 input=sys.stdin.readline
 
-def fastExponentiation(a, x, n):    # a^x mod n
-    y = 1
-    while x > 0:
-        if x & 1  == 1:         # 지수의 LSB가 1인지 확인
-            y = (a * y) % n     # Multiply Operation
-        a = (a * a) % n         # Square Operation
-        x = x >> 1
-    return y
+a,b,c = map(int,input().split())
 
-a,b,c= map(int,input().split())
-rst = fastExponentiation(a,b,c)
+def multi (a,b):
+    if b == 1:
+        return a%c
+    else:
+        n = multi(a,b//2)
+        if b % 2 == 0:
+            return(n*n)%c
+        else:
+            return (n * n * a) % c
 
+rst = multi(a,b)
 print(rst)
 
-#알고리즘 출처
-#https://velog.io/@choihocheol/%EB%B9%A0%EB%A5%B8-%EA%B1%B0%EB%93%AD%EC%A0%9C%EA%B3%B1-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98Fast-Exponentiation-Algorithm
-
-#비트연산 모음
-#https://dypar-study.tistory.com/15
