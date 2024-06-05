82cd953346e80a25f8d3af0a26920b5c2def5a86
lright-psw <52391568+lright-psw@users.noreply.github.com>
2024-06-05 19:36:01 +0900

    [Bronze IV] Title: 주사위 세개, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,17 @@
+# n,m = int(input().split())
+
+# arr = []
+# for _ in range(n):
+#     arr.append(list(map(int,input().split())))
+
+a,b,c = map(int,input().split())
+if a == b and b == c:
+    print(10000+a*1000)
+elif a==b:
+    print(1000+a*100)
+elif b==c:
+    print(1000+b*100)
+elif a==c:
+    print(1000+a*100)
+else:
+    print(100*max(a,b,c))
