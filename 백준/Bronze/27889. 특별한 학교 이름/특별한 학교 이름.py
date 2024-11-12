2c0f3966c7178924ee0dc15a601815cd60495eca
lright-psw <52391568+lright-psw@users.noreply.github.com>
2024-11-12 21:54:03 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+s = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy"
+}
+
+a = input()
+
+print(s[a])
