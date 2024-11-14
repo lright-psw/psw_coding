64289b401bd647f03575b2546033c9c1f933457a
lright-psw <52391568+lright-psw@users.noreply.github.com>
2024-11-14 22:22:18 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,12 @@
+s = {"Algorithm":204,
+"DataAnalysis":207,
+"ArtificialIntelligence":302,
+"CyberSecurity":"B101",
+"Network":303,
+"Startup":501,
+"TestStrategy":105}
+
+n = int(input())
+for i in range(n):
+    a = input()
+    print(s[a])
