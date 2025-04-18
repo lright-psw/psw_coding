76c195d3d96e2be7dc06e5b218046bc1954e068d
lright-psw <52391568+lright-psw@users.noreply.github.com>
2025-04-18 10:13:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 172 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+N = int(input().strip())
+
+num = 666
+cnt = 0
+
+while True:
+    if "666" in str(num):
+        cnt += 1
+        if cnt == N:
+            print(num)
+            break
+    num += 1
