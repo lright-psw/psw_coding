4a2c63a1f3365b421d302b71804b4a850937beee
lright-psw <52391568+lright-psw@users.noreply.github.com>
2025-07-15 22:06:06 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    double rev = n / 4.0;
+    cout << fixed << setprecision(2) << rev << endl;
+    return 0;
+}
