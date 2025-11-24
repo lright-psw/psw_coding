b301fea0b14da913f17d628dd4fb0e8a1d86050d
lright-psw <52391568+lright-psw@users.noreply.github.com>
2025-11-24 16:10:23 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <string>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+       
+    int a = 0;
+    cin >> a;
+
+    cout << pow(2, a);
+
+    return 0;
+}
