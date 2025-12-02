95bb5bb992fad34add3039fb3c2e5477bf53bdbe
lright-psw <52391568+lright-psw@users.noreply.github.com>
2025-12-02 17:43:48 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 110328 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,18 @@
+def main():
+    n, k =map(int,input().split())
+
+    items = []
+    for _ in range(n):
+        w, v = map(int, input().split())
+        items.append((w, v))
+    
+    dp = [0] * (k+1)
+
+    for w, v in items:
+        for i in range(k,w-1,-1):
+            dp[i]= max(dp[i], dp[i-w] + v)
+
+    print(dp[k])
+
+if __name__ == "__main__":
+    main()
